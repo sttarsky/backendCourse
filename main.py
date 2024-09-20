869796f99f969b995385d0dc26ec107e703a4db1
ilya <ilyuash@yandex.ru>
2024-09-20 20:27:41 +0300

    diff sync and async

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import asyncio
+import threading
+import time
+
 import uvicorn
 from fastapi import FastAPI, Query, Body
 
@@ -8,6 +12,21 @@ hotels = [
     {"id": 2, "title": "Dubai", "name": "dubai"}
 ]
 
+
+@app.get("/sync/{id}")
+def sync_func(id: int):
+    print(f"sync. Потоков: {threading.active_count()}")
+    print(f"sync. Начал {id}: {time.time():.2f}")
+    time.sleep(3)
+    print(f"sync. Закончил {id}: {time.time():.2f}")
+
+
+@app.get("/async/{id}")
+async def async_func(id: int):
+    print(f"async. Потоков: {threading.active_count()}")
+    print(f"async. Начал {id}: {time.time():.2f}")
+    await asyncio.sleep(3)
+    print(f"async. Закончил {id}: {time.time():.2f}")
 
 @app.get('/hotels')
 def get_hotels(
