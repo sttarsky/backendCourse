e54ea02b8ed1a3f04b2512117c85dc1b6e4849dc
IluaAir <ilya@air.ru>
2024-10-05 16:36:11 +0300

    fix lower_case with contains

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -19,9 +19,9 @@ async def get_hotels(
     async with async_session_maker() as session:
         query = select(HotelsOrm)
         if location:
-            query = query.filter(func.lower(HotelsOrm.location).like(f'%{location.lower()}%'))
+            query = query.filter(func.lower(HotelsOrm.location).contains(location.strip().lower()))
         if title:
-            query = query.filter(func.lower(HotelsOrm.title).like(f'%{title.lower()}%'))
+            query = query.filter(func.lower(HotelsOrm.title).contains(title.strip().lower()))
         query = (
             query
             .limit(per_page)
