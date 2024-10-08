b67d430374f5e9eac2454405635e0e6c3b502be5
IluaAir <ilya@air.ru>
2024-10-08 13:04:28 +0300

    fix repository post hotels

--- src/repository/hotels.py
+++ src/repository/hotels.py
@@ -22,8 +22,3 @@ class HotelsRepository(BaseRepository):
         )
         result = await self.session.execute(query)
         return result.scalars().all()
-
-    async def add(self, hotel: Hotel):
-        add_hotel_stmt = insert(self.model).values(hotel.model_dump())
-        result = await self.session.execute(add_hotel_stmt)
-        return result
