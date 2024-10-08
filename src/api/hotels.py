ede07221d19326f553f12782bbbd1778e71eb6f3
IluaAir <ilya@air.ru>
2024-10-08 12:44:31 +0300

    add post

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -45,11 +45,9 @@ async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
     }
 })):
     async with async_session_maker() as session:
-        add_hotel_stmt = insert(HotelsOrm).values(**hotel_data.model_dump())
-        print(add_hotel_stmt.compile(bind=engine, compile_kwargs={'literal_binds': True}))
-        await session.execute(add_hotel_stmt)
+        hotel = await HotelsRepository(session).add(hotel_data)
         await session.commit()
-    return {"status": "ok"}
+    return {"status": "OK", "data": hotel}
 
 
 @router.put("/{hotel_id}")
--- src/repository/hotels.py
+++ src/repository/hotels.py
@@ -1,7 +1,9 @@
 from sqlalchemy import select, func
+from sqlalchemy.dialects.mysql import insert
 
 from src.models.hotels import HotelsOrm
 from src.repository.base import BaseRepository
+from src.schemas.hotels import Hotel
 
 
 class HotelsRepository(BaseRepository):
@@ -20,3 +22,8 @@ class HotelsRepository(BaseRepository):
         )
         result = await self.session.execute(query)
         return result.scalars().all()
+
+    async def add(self, hotel: Hotel):
+        add_hotel_stmt = insert(self.model).values(hotel.model_dump())
+        result = await self.session.execute(add_hotel_stmt)
+        return result
