f2577ed66a1a6819bfb0c160c803b9e750b84ae3
IluaAir <ilya@air.ru>
2024-10-10 20:16:45 +0300

    add users.py

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -9,6 +9,8 @@ from src.config import settings
 from src.databases import Base
 from src.models.hotels import HotelsOrm # noqa
 from src.models.rooms import RoomsOrm # noqa
+from src.models.users import UsersORM # noqa
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
---
+++ src/models/users.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+from src.databases import Base
+
+
+class UsersORM(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    surname: Mapped[str] = mapped_column(String(20))
+    nickname: Mapped[str] = mapped_column(String(50))
+    email: Mapped[str] = mapped_column(String(200))
+    hashed_password: Mapped[str] = mapped_column(String(200))
