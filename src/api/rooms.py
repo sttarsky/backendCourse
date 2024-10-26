b5996dd0f354920c9110ca9b602ec51acee844be
IluaAir <ilya@air.ru>
2024-10-26 12:42:51 +0300

    fix rooms api

--- src/api/rooms.py
+++ src/api/rooms.py
@@ -1,9 +1,8 @@
-
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Body
 
 from src.databases import async_session_maker
 from src.repository.rooms import RoomsRepository
-from src.schemas.rooms import RoomADD, RoomPATCH
+from src.schemas.rooms import RoomADD, RoomPATCH, RoomADDRequest, RoomPATCHRequest
 
 router = APIRouter(prefix='/hotels', tags=['Номера'])
 
@@ -11,48 +10,49 @@ router = APIRouter(prefix='/hotels', tags=['Номера'])
 @router.get('/{hotel_id}/rooms')
 async def get_rooms(hotel_id: int):
     async with async_session_maker() as session:
-        result = await RoomsRepository(session).get_all(hotel_id)
+        result = await RoomsRepository(session).get_filtered(hotel_id=hotel_id)
         return result
 
 
 @router.get('/{hotel_id}/rooms/{room_id}')
 async def get_one_room(hotel_id: int, room_id: int):
     async with async_session_maker() as session:
-        result = await RoomsRepository(session).get_one_or_none(hotel_id, room_id)
+        result = await RoomsRepository(session).get_one_or_none(id=room_id, hotel_id=hotel_id)
         if not result:
             raise HTTPException(status_code=404, detail='Room not found')
         return result
 
 
 @router.post("/{hotel_id}/rooms")
-async def create_room(room_data: RoomADD):
+async def create_room(hotel_id: int, room_data: RoomADDRequest = Body()):
+    _room_data = RoomADD(hotel_id=hotel_id, **room_data.model_dump())
     async with async_session_maker() as session:
-        room = await RoomsRepository(session).add(room_data)
+        room = await RoomsRepository(session).add(_room_data)
         await session.commit()
     return {"status": "OK", "data": room}
 
 
-@router.put("/{hotel_id}/rooms")
-async def put_hotel(hotel_id: int, room_data: RoomADD):
+@router.put("/{hotel_id}/rooms/{room_id}")
+async def put_room_id(hotel_id: int, room_id: int, room_data: RoomADDRequest):
+    _room_data = RoomADD(hotel_id=hotel_id, **room_data.model_dump())
     async with async_session_maker() as session:
-        await RoomsRepository(session).edit(room_data, id=hotel_id)
+        await RoomsRepository(session).edit(_room_data, id=room_id)
         await session.commit()
         return {"status": "ok"}
 
 
-@router.patch("/{room_id}")
-async def patch_hotel(room_id: int, room_data: RoomPATCH):
+@router.patch("/{hotel_id}/rooms/{room_id}")
+async def patch_hotel(hotel_id: int, room_id: int, room_data: RoomPATCHRequest):
+    _room_data = RoomPATCH(hotel_id=hotel_id, **room_data.model_dump(exclude_unset=True))
     async with async_session_maker() as session:
-        await RoomsRepository(session).edit(room_data, exclude_unset=True, id=room_id)
+        await RoomsRepository(session).edit(_room_data, exclude_unset=True, hotel_id=hotel_id, id=room_id)
         await session.commit()
         return {"status": "ok"}
 
 
-@router.delete("/{hotel_id}")
-async def del_hotel(hotel_id: int):
+@router.delete("/{hotel_id}/rooms/{room_id}")
+async def del_hotel(hotel_id: int, room_id: int):
     async with async_session_maker() as session:
-        await RoomsRepository(session).delete(id=hotel_id)
+        await RoomsRepository(session).delete(id=room_id, hotel_id=hotel_id)
         await session.commit()
         return {"status": "ok"}
-
-
--- src/schemas/rooms.py
+++ src/schemas/rooms.py
@@ -1,22 +1,38 @@
 from pydantic import BaseModel, ConfigDict, Field
+
 from src.schemas.hotels import Hotel
 
 
-class RoomADD(BaseModel):
-    hotel_id: Hotel
+class RoomADDRequest(BaseModel):
     title: str
     description: str | None = Field(None)
     price: int
     quantity: int
 
 
-class Rooms(BaseModel):
+class RoomADD(BaseModel):
+    hotel_id: int
+    title: str
+    description: str | None = Field(None)
+    price: int
+    quantity: int
+
+
+class Rooms(RoomADD):
     id: int
 
     model_config = ConfigDict(from_attributes=True)
 
 
+class RoomPATCHRequest(BaseModel):
+    title: str | None = Field(None)
+    description: str | None = Field(None)
+    price: int | None = Field(None)
+    quantity: int | None = Field(None)
+
+
 class RoomPATCH(BaseModel):
+    hotel_id: int | None = Field(None)
     title: str | None = Field(None)
     description: str | None = Field(None)
     price: int | None = Field(None)
