ebc748e714c7edce1e5e4fa7cbb10ce377f3270c
IluaAir <ilya@air.ru>
2024-11-01 19:51:34 +0300

    add get my bookings and all bookings

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -1,9 +1,15 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 
 from src.api.dependencies import DBDep, UserIdDep
 from src.schemas.bookings import BookingADD, BookingADDRequest
 
 router = APIRouter(prefix="/bookings", tags=["Бронирование"])
+
+
+@router.get("/me")
+async def get_my_bookings(user_id: UserIdDep, db: DBDep):
+    bookings = await db.bookings.get_filtered(user_id=user_id)
+    return bookings
 
 
 @router.post("")
@@ -14,3 +20,8 @@ async def post_booking(user_id: UserIdDep, booking_data: BookingADDRequest, db: 
     booking = await db.bookings.add(_booking_data)
     await db.commit()
     return {"status": "ok", "booking": booking}
+
+
+@router.get("")
+async def get_all_bookings(db: DBDep):
+    return await db.bookings.get_all()
