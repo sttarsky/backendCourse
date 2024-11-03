3f6cc4dcb951a405473af8ebc352c67767744bdd
IluaAir <ilya@air.ru>
2024-11-03 14:30:47 +0300

    create query for checking free rooms

--- src/api/rooms.py
+++ src/api/rooms.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, HTTPException, Body
+from datetime import date
+
+from fastapi import APIRouter, HTTPException, Body, Query
 from src.api.dependencies import DBDep
 from src.schemas.rooms import RoomADD, RoomPATCH, RoomADDRequest, RoomPATCHRequest
 
@@ -6,8 +8,15 @@ router = APIRouter(prefix='/hotels', tags=['Номера'])
 
 
 @router.get('/{hotel_id}/rooms')
-async def get_rooms(hotel_id: int, db: DBDep):
-    return await db.rooms.get_filtered(hotel_id=hotel_id)
+async def get_rooms(hotel_id: int,
+                    db: DBDep,
+                    date_from: date = Query(example="2024-11-01"),
+                    date_to: date = Query(example="2024-11-30")
+                    ):
+    return await db.rooms.get_filtered_by_time(hotel_id=hotel_id,
+                                               date_from=date_from,
+                                               date_to=date_to
+                                               )
 
 
 @router.get('/{hotel_id}/rooms/{room_id}')
--- src/repository/rooms.py
+++ src/repository/rooms.py
@@ -1,3 +1,8 @@
+from datetime import date
+from sqlalchemy import select, func
+
+from src.databases import engine
+from src.models.bookings import BookingsORM
 from src.models.rooms import RoomsOrm
 from src.repository.base import BaseRepository
 from src.schemas.rooms import Rooms
@@ -7,3 +12,52 @@ class RoomsRepository(BaseRepository):
     model = RoomsOrm
     schema = Rooms
 
+    async def get_filtered_by_time(self,
+                                   hotel_id: int,
+                                   date_from: date,
+                                   date_to: date
+                                   ):
+
+        """
+        !with rooms_booked as (
+            select bookings.room_id, count(*) as booked
+            from bookings
+            where date_from <= '2034-12-14' and date_to >= '2034-11-01'
+            group by room_id
+        )
+        """
+        rooms_booked = (select(BookingsORM.room_id, func.count("*").label("booked"))
+                        .select_from(BookingsORM)
+                        .filter(BookingsORM.date_from <= date_to,
+                                BookingsORM.date_to >= date_from)
+                        .group_by(BookingsORM.room_id)
+                        .cte(name="rooms_booked")
+                        )
+        """
+        rooms_purches as (
+            select rooms.id as room_id, rooms.quantity,  quantity - coalesce(booked, 0) as rooms_left
+            from rooms
+            left join rooms_booked
+            on rooms_booked.room_id = rooms.id
+        )
+        """
+        rooms_purches = (
+            select(
+                self.model.id.label("room_id"),
+                (self.model.quantity - func.coalesce(rooms_booked.c.booked, 0)).label("rooms_left")
+            )
+            .select_from(self.model)
+            .outerjoin(rooms_booked, self.model.id == rooms_booked.c.room_id)
+            .cte("rooms_purches")
+        )
+        """
+        select *
+        from rooms_purches
+        where rooms_left > 0;
+        """
+        query = (
+            select("*")
+            .select_from(rooms_purches)
+            .filter(rooms_purches.c.rooms_left > 0)
+        )
+        print(query.compile(bind=engine, compile_kwargs={"literal_binds": True}))
