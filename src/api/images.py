c891a5b6db050baaf7bc6adfddc0f7dba70cfc04
IluaAir <ilya@air.ru>
2024-12-13 18:07:08 +0300

    add example of Background task

--- src/api/images.py
+++ src/api/images.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, UploadFile, BackgroundTasks
 from src.tasks.tasks import resize_image
 import shutil
 
@@ -6,9 +6,9 @@ router = APIRouter(prefix='/images', tags=['Изображения отелей'
 
 
 @router.post("")
-def upload_image(file: UploadFile):
+def upload_image(file: UploadFile, background_tasks: BackgroundTasks):
     image_path = f"src/static/images/{file.filename}"
     with open(image_path, "wb+") as new_file:
         shutil.copyfileobj(fsrc=file.file, fdst=new_file)
-
-    resize_image.delay(image_path)
+    background_tasks.add_task(resize_image, image_path)
+    # resize_image.delay(image_path)
