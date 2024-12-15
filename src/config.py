890890d20b04208b0558e3e78aa57281fbdb8650
IluaAir <ilya@air.ru>
2024-12-15 13:54:19 +0300

    add pytest second conf

--- src/config.py
+++ src/config.py
@@ -1,7 +1,10 @@
+from typing import Literal
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    MODE: Literal["TEST", "DEV", "PROD"]
     DB_NAME: str
     DB_IP: str
     DB_PORT: int
