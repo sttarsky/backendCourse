8bcf38167ee1fffc4268c6dde00d663dde2d8280
IluaAir <ilya@air.ru>
2024-12-16 16:43:36 +0300

    add fixture create hotels and rooms

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
+import json
+
 from httpx import AsyncClient
 import pytest
 
 from src.config import settings
-from src.databases import Base, engine_null_pool
+from src.databases import Base, engine_null_pool, async_null_session_maker
 from src.main import app
 from src.models import *  # noqa
+from src.schemas.hotels import HotelADD
+from src.schemas.rooms import RoomADD
+from src.utils.db_manager import DBManager
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -20,6 +25,22 @@ async def setup_database(check_db):
 
 
 @pytest.fixture(scope="session", autouse=True)
+async def create_hotels_rooms(setup_database):
+    mock_hotels = "tests/mock_hotels.json"
+    mock_rooms = "tests/mock_rooms.json"
+    with open(mock_hotels, 'r') as file:
+        hotels_to_incert = json.load(file)
+    with open(mock_rooms, 'r') as file:
+        rooms_to_incert = json.load(file)
+    hotels = [HotelADD.model_validate(hotel) for hotel in hotels_to_incert]
+    rooms = [RoomADD.model_validate(room) for room in rooms_to_incert]
+    async with DBManager(session_factory=async_null_session_maker) as db:
+        await db.hotels.add_bulk(hotels)
+        await db.rooms.add_bulk(rooms)
+        await db.commit()
+
+
+@pytest.fixture(scope="session", autouse=True)
 async def register_user(setup_database):
     async with AsyncClient(app=app, base_url="http://test") as ac:
         result = await ac.post(
@@ -32,4 +53,3 @@ async def register_user(setup_database):
 
             }
         )
-        print(result)
