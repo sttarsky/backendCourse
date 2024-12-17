2c1acbb27cd10200f3b6465c01acf1c71225dc0a
IluaAir <ilya@air.ru>
2024-12-17 19:50:36 +0300

    add crud check for booking

--- tests/integration_tests/bookings/test_db.py
+++ tests/integration_tests/bookings/test_db.py
@@ -6,7 +6,7 @@ from asyncpg.pgproto.pgproto import timedelta
 from src.schemas.bookings import BookingADD
 
 
-async def test_add_hotel(db):
+async def test_booking_crud(db):
     user = (await db.users.get_all())[0].id
     room = (await db.rooms.get_all())[0].id
     booking = BookingADD(
@@ -16,6 +16,10 @@ async def test_add_hotel(db):
         date_to=date.today() + timedelta(days=1),
         price=randint(100, 2000)
     )
-    new_booking = await db.bookings.add(booking)
-    await db.commit()
-    print(f"{new_booking=}")
+    assert await db.bookings.add(booking)
+    booking.date_to = date.today() + timedelta(days=randint(15, 25))
+    await db.bookings.edit(data=booking, exclude_unset=True)
+    print(f"booking={booking.model_dump()}")
+    assert await db.bookings.get_one_or_none(**booking.model_dump())
+    await db.bookings.delete(**booking.model_dump())
+    assert (await db.bookings.get_one_or_none(**booking.model_dump())) is None
