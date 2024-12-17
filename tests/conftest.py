173cb4c468dda2e0f994dd564eb25fa8b23211ba
IluaAir <ilya@air.ru>
2024-12-17 20:39:29 +0300

    update async-null-pool

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import json
 from httpx import AsyncClient
 import pytest
 
+from src.api.dependencies import get_db
 from src.config import settings
 from src.databases import Base, engine_null_pool, async_null_session_maker
 from src.main import app
@@ -12,6 +13,11 @@ from src.schemas.rooms import RoomADD
 from src.utils.db_manager import DBManager
 
 
+async def get_db_null_pool():
+    async with DBManager(session_factory=async_null_session_maker) as db:
+        yield db
+
+
 @pytest.fixture(scope="session", autouse=True)
 async def check_db():
     assert settings.MODE == "TEST"
@@ -19,14 +25,18 @@ async def check_db():
 
 @pytest.fixture(scope="function")
 async def db() -> DBManager:
-    async with DBManager(async_null_session_maker) as db:
+    async for db in get_db_null_pool():
         yield db
+
+
+app.dependency_overrides[get_db] = get_db_null_pool
 
 
 @pytest.fixture(scope="session")
 async def ac() -> AsyncClient:
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        yield ac
+    async with app.router.lifespan_context(app):
+        async with AsyncClient(app=app, base_url="http://test") as ac:
+            yield ac
 
 
 @pytest.fixture(scope="session", autouse=True)
