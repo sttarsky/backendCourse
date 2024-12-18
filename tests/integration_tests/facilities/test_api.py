af57a99f92429c7837a6513fb6bf558ff450922e
IluaAir <ilya@air.ru>
2024-12-18 20:21:27 +0300

    fix test facilities

--- tests/integration_tests/facilities/test_api.py
+++ tests/integration_tests/facilities/test_api.py
@@ -1,12 +1,14 @@
-
-async def test_api_facilities(ac):
-    response = await ac.post(
-        "/facilities",
-        json={
-            "title": "Wi-Fi"
-        }
-    )
-    print(f"{response.json()=}")
+async def test_get_facilities(ac):
+    response = await ac.get("/facilities")
     assert response.status_code == 200
-    assert await ac.get("/facilities")
+    assert isinstance(response.json(), list)
 
+
+async def test_post_facilities(ac):
+    facility_title = "Массаж"
+    response = await ac.post("/facilities", json={"title": facility_title})
+    assert response.status_code == 200
+    res = response.json()
+    assert isinstance(res, dict)
+    assert res["data"]["title"] == facility_title
+    assert "data" in res
